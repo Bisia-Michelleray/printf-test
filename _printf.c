b671b4882c0609156d7e8849d961a82d50876847
Bisia Michelleray <bisiamitch7@gmail.com>
2023-06-26 19:29:39 -0700

    hope it works

--- _printf.c
+++ _printf.c
@@ -17,9 +17,7 @@ int _printf(const char *format, ...)
 		 va_list args;
 
 		 va_start(args, format);
-		if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-			return (-1);
-		if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
+		if (format == NULL || (format[0] == '%' && format[1] == '\0') || format[1] == ' ')
 			return (-1);
 		while (format[i])
 		{
